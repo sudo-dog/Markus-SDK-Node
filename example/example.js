ab7d418763e1978d91748b597c019c49c4ed03a3
PCXPY <pc@wmpcxpy.com>
2018-07-20 20:20:13 -0500

    [Improvement] Add multi-upload func

--- example/example.js
+++ example/example.js
@@ -1,4 +1,10 @@
 const Markus = require('../dist/index').default;
 const fs = require('fs');
+
+const buffers = []
+for (let i = 36; i < 99; i++) {
+    buffers.push(fs.readFileSync('../../../test/IMG_30' + i + '.JPG'));
+}
+
 const a = new Markus('http://localhost:8080');
-a.UploadSingleBuffer(buffer);
+a.UploadMultipleBuffer(buffers);
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ export default class Markus {
         return new Promise<any>((resolve, reject) => {
             var r = request.post(this.domain + '/m/buffer', (error, response, body) => {
                 if (!error && response.statusCode == 200) {
+                    resolve(body);
                     console.log(body);
                 } else {
                     console.log(error, body);
@@ -23,6 +24,31 @@ export default class Markus {
             });
             form.append('tags', JSON.stringify(['sdk']));
             form.append('key', 'test');
-        })
+        });
+    }
+
+    public UploadMultipleBuffer(buffers: Buffer[]): Promise<any> {
+        const resultList: any[] = [];
+        const Upload = (cb: () => any) => {
+            const buffer = buffers.shift();
+            if (buffer) {
+                this.UploadSingleBuffer(buffer).then((result: any) => {
+                    resultList.push(result);
+                    Upload(cb);
+                })
+            } else {
+                cb();
+            }
+        }
+        return new Promise<any>((resolve, reject) => {
+            const loop = buffers.length <= 5 ? buffers.length : 5;
+            for (let i = 0; i < loop; i++) {
+                Upload(() => {
+                    if (resultList.length === buffers.length) {
+                        console.log(resultList);
+                    }
+                });
+            }
+        });
     }
 }
